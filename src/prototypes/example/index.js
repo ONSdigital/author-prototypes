95789cce1e0517fc3e10af690c231348f5ea25fe
LaurenDowling <108019533+LaurenDowling@users.noreply.github.com>
2023-06-05 17:42:54 +0100

    fixing scripts for forgotten password

--- src/prototypes/example/index.js
+++ src/prototypes/example/index.js
@@ -2,3 +2,51 @@
 Imports question manager, summary generator, answer piping and previous link history
 ************************************************************************************/
 import '@ons/prototype-kit/src/helpers/index.js';
+
+function validateForm_ForgotPasswordEmail(){
+    console.log('run');
+    let b = document.forms["myForm"]["email"].value;
+    let c = document.forms["myForm"]["Confirmemail"].value;
+
+    let y = b.trim();
+    let z = c.trim();
+
+    sessionStorage.setItem('ForgotPasswordEmail', y);    
+    sessionStorage.setItem('ForgotPasswordConfirmEmail', z);   
+
+    if ((y != "") && (z != "") && (y == z) && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == true)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/CheckEmail.html";    
+    }
+    if ((y != "") && (z == "") && (y.indexOf("@") == -1)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/InvalidEmail_NoConfirm.html";    
+    }
+    if ((y != "") && (z != "") && (y != z) && (y.indexOf("@") == -1)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/InvalidEmail_NotMatching.html";    
+    }
+    if ((y != "") && (z != "") && (y == z) && (y.indexOf("@") == -1)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/InvalidEmail.html";    
+    }
+    if ((y != "") && (z == "") && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == true)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NoConfirm.html";    
+    }
+    if ((y == "") && (z != "")) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NoEmail.html";    
+    }
+    if ((y == "") && (z == "")) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NoEmailNoConfirm.html";    
+    }
+    if ((y != "") && (z != "") && (y != z) && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == true)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NotMatching.html";    
+    }
+    if ((y != "") && (z == "") && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == false)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NotONSEmail_NoConfirm.html";    
+    }
+    if ((y != "") && (z != "") && (y != z) && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == false)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NotONSEmail_NotMatching.html";    
+    }
+    if ((y != "") && (z != "") && (y == z) && (y.indexOf("@") != -1) && (y.includes("@ons.gov.uk") == false)) {
+        document.forms["myForm"].action = "/prototypes/example/login_journeys/ForgotPassword/errors/Email/NotONSEmail.html";    
+    }
+}
+
+window.validateForm_ForgotPasswordEmail = validateForm_ForgotPasswordEmail
